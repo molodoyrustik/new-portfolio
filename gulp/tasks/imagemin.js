91980011ff63e00191fe01a06a0c59bfc7215e62
Ruslan Bagautinov <molodoybaja@gmail.com>
2019-06-15 21:52:52 +0500

    доделал слайдер с флоатами

--- gulp/tasks/imagemin.js
+++ gulp/tasks/imagemin.js
@@ -2,7 +2,7 @@ module.exports = function() {
   $.gulp.task('imagemin', function() {
     return $.gulp.src('./src/images/**/*.{png,jpg,svg}')
       .pipe($.gp.imagemin([
-        $.gp.imagemin.optipng({optimizationLevel: 5}),
+        // $.gp.imagemin.optipng({optimizationLevel: 5}),
         $.gp.imagemin.jpegtran({progressive: true}),
         $.gp.imagemin.svgo()
       ]))
